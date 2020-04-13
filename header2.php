580971450e6da6a458e6f5430bbcc2de40e00555
unknown <gustavo.vega.droguett@gmail.com>
2020-04-13 16:18:28 -0400

    codigo salvado y eliminando controles innecesarios

--- header2.php
+++ header2.php
@@ -1,4 +1,5 @@
-<?php session_start(); ?>
+<?php session_start();
+include "db.php"; ?>
 
 <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -22,105 +23,92 @@
 <!--partial:index.partial.html -->
 <header>
 
-
-
-
 <div class="contenedor">
-
-        <div class="menu_bar">
-
-        <a href="#" class="bt-menu"><span class="icon-menu"></span> </a>
-
-        </div>  
-       <div class="navbar-leftx" >
-          <a href="index.php"> <img src="img/logo_frozza.png" alt="Cloudy Sky"></a>
-       </div>
+   
+    <div class="navbar-leftx" >
+      <a href="index.php"> <img src="img/logo_frozza.png" alt="Cloudy Sky"></a>
+    </div>
  
     <div class="divbuscardor">  
-          
-        <input type="text"  placeholder="  BUSCADOR">  
-     </div>
+      <input type="text"  placeholder="  BUSCADOR">  
+    </div>
 
     <div class="quelepasalupita">
-    <img src="img/lupa.png" alt="Cloudy Sky">
-    
+     <img src="img/lupa.png" alt="Cloudy Sky">
     </div>
+    
     <?php
-                             include "db.php";
-                             if(!isset($_SESSION["uid"])){
-                               $_SESSION["uid"]=-1;
-                             }
-                            if(isset($_SESSION["uid"]) && $_SESSION["uid"]!=-1){
-                                $sql = "SELECT * FROM usuarios WHERE id_usuario='$_SESSION[uid]'";
-                                $query = mysqli_query($con,$sql);
-                                $row=mysqli_fetch_array($query);  
-                                
-                                echo '
-                               <div class="dropdownn" id="loged">
-                                     <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> Hola '.$row["nombre"].'    </a>
-                                    <div class="dropdownn-content">
-                                    <a href="perfil.php" ><i class="fa fa-user-circle" aria-hidden="true" ></i>Mi Perfil</a>
-                                    <a href="logout.php"  ><i class="fa fa-sign-in" aria-hidden="true"></i>Salir</a>
-                                    
-                                  </div>
-                                </div>';
+            
+            if(!isset($_SESSION["uid"])){
+              $_SESSION["uid"]=-1;
+            }
+          if(isset($_SESSION["uid"]) && $_SESSION["uid"]!=-1){
+              $sql = "SELECT * FROM usuarios WHERE id_usuario='$_SESSION[uid]'";
+              $query = mysqli_query($con,$sql);
+              $row=mysqli_fetch_array($query);  
+              
+              echo '
+              <div class="dropdownn" id="loged">
+                    <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> Hola '.$row["nombre"].'    </a>
+                  <div class="dropdownn-content">
+                  <a href="perfil.php" ><i class="fa fa-user-circle" aria-hidden="true" ></i>Mi Perfil</a>
+                  <a href="logout.php"  ><i class="fa fa-sign-in" aria-hidden="true"></i>Salir</a>
+                  
+                </div>
+              </div>';
 
-                            
-                            }  
-                            else if($_SESSION["uid"]==-1){ 
-                              $sql = "SELECT * FROM usuarios WHERE id_usuario='$_SESSION[uid]'";
-                              $query = mysqli_query($con,$sql);
-                              $row=mysqli_fetch_array($query);    
-                                echo '
-                                <div class="dropdownn" id="loged">
-                                  <a href="" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> '.$row["nombre"].'   '.$_SESSION["uid"].'</a>
-                                  <div class="dropdownn-content">
-                                    <a href="" data-toggle="modal" data-target="#Modal_login"><i class="fa fa-sign-in" aria-hidden="true" ></i>Ingresar</a>
-                                    <a href="" data-toggle="modal" data-target="#Modal_register"><i class="fa fa-user-plus" aria-hidden="true"></i>Registrarse</a>
-                                    
-                                  </div>
-                                </div>';
-                                
-                            }
-                            else{
-                              echo '
-                                <div class="dropdownn" id="loged">
-                                  <a href="" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> Mal entrado</a>
-                                  <div class="dropdownn-content">
-                                    <a href="" data-toggle="modal" data-target="#Modal_login"><i class="fa fa-sign-in" aria-hidden="true" ></i>Ingresar</a>
-                                    <a href="" data-toggle="modal" data-target="#Modal_register"><i class="fa fa-user-plus" aria-hidden="true"></i>Registrarse</a>
-                                    
-                                  </div>
-                                </div>';
-                              
-                            }
+          
+          }  
+          else if($_SESSION["uid"]==-1){ 
+            $sql = "SELECT * FROM usuarios WHERE id_usuario='$_SESSION[uid]'";
+            $query = mysqli_query($con,$sql);
+            $row=mysqli_fetch_array($query);    
+              echo '
+              <div class="dropdownn" id="loged">
+                <a href="" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> '.$row["nombre"].'   '.$_SESSION["uid"].'</a>
+                <div class="dropdownn-content">
+                  <a href="" data-toggle="modal" data-target="#Modal_login"><i class="fa fa-sign-in" aria-hidden="true" ></i>Ingresar</a>
+                  <a href="" data-toggle="modal" data-target="#Modal_register"><i class="fa fa-user-plus" aria-hidden="true"></i>Registrarse</a>
+                  
+                </div>
+              </div>';
+              
+          }
+          else{
+            echo '
+              <div class="dropdownn" id="loged">
+                <a href="" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> Mal entrado</a>
+                <div class="dropdownn-content">
+                  <a href="" data-toggle="modal" data-target="#Modal_login"><i class="fa fa-sign-in" aria-hidden="true" ></i>Ingresar</a>
+                  <a href="" data-toggle="modal" data-target="#Modal_register"><i class="fa fa-user-plus" aria-hidden="true"></i>Registrarse</a>
+                  
+                </div>
+              </div>';
+            
+          }
                                              ?>
 
-<div class="navbar-rightx">
-           <a href="#" id="cart">
-      
-           <img src="img/carrito.png" alt="Cloudy Sky" class="imgcarro"></a>
-             </i><span class="badge">0</span>
-    
-           <div class="shopping-cart">
-           <div class="shopping-cart-header">
-     
-            <img class="torito" src="img/torito_rojo.png" alt="item1" /><span class="badge2">0</span>
-            
-             </div>
-            <div class="cart-dropdown" id="pruebaCarro">
-                <div class="cart-list" id="cart_product">
+    <div class="navbar-rightx">   <!-- Carrito -->
+      <a href="#" id="cart">  <img src="img/carrito.png" alt="Cloudy Sky" class="imgcarro"> </a>
+      <span class="badge">0</span>
+        
+        <div class="shopping-cart">
+          <div class="shopping-cart-header">
+            <img class="torito" src="img/torito_rojo.png" alt="item1" > <span class="badge2">0</span> </img>
+                
+          </div>
+          <div class="cart-dropdown" id="pruebaCarro">
+            <div class="cart-list" id="cart_product">
 
-                </div>        
-                <div class="cart-btns">
-                     <a href="cart.php" style="width:100%;"><i class="fa fa-edit"></i> Ir a Carrito</a>
-              
-                </div>
+            </div>        
+            <div class="cart-btns">
+              <a href="cart.php" style="width:100%;"><i class="fa fa-edit"></i> Ir a Carrito</a>
+            </div>
           </div>
-          </div>
-          </div>  
+        </div>
+    </div>  <!-- Carrito fin -->
           
-    <nav class="menu-principal">
+    <nav class="menu-principal">    <!-- Menu anidado -->
       <ul>
 
         <li> <a href="index.php"><span class="icon-power-cord"></span> INICIO</a></li>
@@ -144,7 +132,7 @@
           <li> <a href="lista_pro_vista.php"><span class="icon-switch"></span>TODOS LOS PROD</a></li>
         </ul>
 
-    </nav>
+    </nav>  <!-- Fin de menu anidado-->
 
            
     
@@ -208,7 +196,7 @@
                         
     </div>
 
-</div>
+    </div>
 </div>
 
 
